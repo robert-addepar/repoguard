36682896d2d3293a6b8eddcaa8aa70746cf17664
Robert Picard <robert.picard@addepar.com>
2015-11-05 16:23:26 -0800

    debug

--- repoguard.py
+++ repoguard.py
@@ -437,6 +437,7 @@ class RepoGuard:
             sys.exit()
 
     def run(self):
+        print("HELLO. IS IT ME YOU'RE LOOKING FOR?")
         self.logger.info('* run started')
         self.logger.debug('Called with arguments: %s' % self.args)
 
